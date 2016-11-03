cc98a513479b52a4201fdc5c3b195f306a467050
Rodion Gorkovenko <RodionGork@github.com>
2016-11-03 08:53:07 +0300

    VK auth widget and dot templates

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const app = express();
+const engine = require('express-dot-engine');
 
 const statics = require('./statics');
 
-const HTTP_PORT = 8080;
+const HTTP_PORT = process.env.port || 8080;
+
+app.engine('html', engine.__express);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
 
 function rootHandler(req, res) {
-    res.send('<h1>Hi, Peoplez!</h1>');
+    res.render('index.html', {secret: new Date().getTime(), host: req.get('host')});
+}
+
+function authHandler(req, res) {
+    res.send(req.query);
 }
 
 statics(app);
 app.get('/', rootHandler);
+app.get('/auth', authHandler);
 app.get('/test/:name/:age', (req, res) => {
     res.send('Name is ' + req.params['name'] + ', age is ' + req.params['age']);
 });
