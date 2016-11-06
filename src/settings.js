af8a00bafc237d5b8bf10d57e601590192d7b27f
RodionGork <rodiongork@github.com>
2016-11-06 16:06:21 +0300

    ESLint more rules

--- src/settings.js
+++ src/settings.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
 
 let file = null;
+const devType = process.env.dev_type || 'dev';
 
 try {
-    file = fs.readFileSync(__dirname + '/../secrets.json');
+    file = fs.readFileSync(__dirname + '/../settings.json');
 } catch (e) {
-    file = fs.readFileSync(__dirname + '/../secrets-stub.json');
+    file = fs.readFileSync(__dirname + '/../settings-stub.json');
 }
 
 const json = JSON.parse(file);
+const result = json[devType];
+result.devType = devType;
 
-module.exports = json[process.env.dev_type || 'dev'];
+module.exports = result;
 
